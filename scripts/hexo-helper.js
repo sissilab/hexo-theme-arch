677aa56edae9fa2c2dd56f1638e9e156feaef6ec
sissilab <xiangru.au@gmail.com>
2023-04-07 22:54:13 +0800

    support to switch prev/next page based on different languages

--- scripts/hexo-helper.js
+++ scripts/hexo-helper.js
@@ -19,6 +19,55 @@ function switchLang(lang) {
   return '';
 }
 
+function hasPagePrev(currentPage) {
+  const lang = currentPage.lang;
+  let prevPage = currentPage.prev
+  if (!lang)
+    return true;
+  while (prevPage && prevPage.lang !== lang) {
+    prevPage = prevPage.prev;
+  }
+  return !!prevPage;
+}
+
+function hasPageNext(currentPage) {
+  const lang = currentPage.lang;
+  let nextPage = currentPage.next
+  if (!lang)
+    return true;
+  while (nextPage && nextPage.lang !== lang) {
+    nextPage = nextPage.next;
+  }
+  return !!nextPage;
+}
+
+function getPagePrevPath(currentPage) {
+  const lang = currentPage.lang;
+  let prevPage = currentPage.prev
+  if (!lang)
+    return prevPage?.path;
+  while (prevPage && prevPage.lang !== lang) {
+    prevPage = prevPage.prev;
+  }
+  return prevPage ? prevPage.path : '/' + lang;
+}
+
+function getPageNextPath(currentPage) {
+  const lang = currentPage.lang;
+  let nextPage = currentPage.next
+  if (!lang)
+    return nextPage?.path;
+  while (nextPage && nextPage.lang !== lang) {
+    nextPage = nextPage.next;
+  }
+  return nextPage ? nextPage.path : '/' + lang;
+}
+
 hexo.extend.helper.register('is_root', isRoot);
 hexo.extend.helper.register('get_langs', getLangs);
 hexo.extend.helper.register('switch_lang', switchLang);
+
+hexo.extend.helper.register('has_page_prev', hasPagePrev);
+hexo.extend.helper.register('has_page_next', hasPageNext);
+hexo.extend.helper.register('get_page_prev_path', getPagePrevPath);
+hexo.extend.helper.register('get_page_next_path', getPageNextPath);
